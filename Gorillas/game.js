480430c62472849e75410fafbd95b6fd83de79bf
dangerpwnd <eric@dangerstone.com>
2024-02-09 15:40:47 -0600

    Added Gorilla game to project

---
+++ Gorillas/game.js
@@ -0,0 +1,243 @@
+let state = {}; // State of the game
+
+// Main canvas element
+const canvas = document.getElementById("game");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
+
+newGame();
+
+function newGame() {
+  // Reset state
+  state = {
+    phase: "aiming", // aiming, in flight, celebrating
+    currentPlayer: 1,
+    bomb: {
+      x: undefined,
+      y: undefined,
+      rotation: 0,
+      velocity: { x: 0, y: 0 },
+    },
+
+    // Setting
+    backgroundBuildings: [],
+    buildings: [],
+    blastholes: [],
+  };
+
+  for (let i = 0; i < 11; i++) {
+    generateBackgroundBuildings(i);
+  }
+
+  for (let i = 0; i < 8; i++) {
+    generateBuilding(i);
+  }
+}
+
+draw();
+
+function generateBackgroundBuildings(index) {
+  const previousBuilding = state.backgroundBuildings[index - 1];
+
+  const x = previousBuilding
+    ? previousBuilding.x + previousBuilding.width + 4
+    : -30;
+
+  const minWidth = 60;
+  const maxWidth = 110;
+  const width = minWidth + Math.random() * (maxWidth - minWidth);
+
+  const minHeight = 80;
+  const maxHeight = 350;
+  const height = minHeight + Math.random() * (maxHeight - minHeight);
+
+  state.backgroundBuildings.push({ x, width, height });
+}
+
+function generateBuilding(index) {
+  const previousBuilding = state.backgroundBuildings[index - 1];
+
+  const x = previousBuilding
+    ? previousBuilding.x + previousBuilding.width + 4
+    : 0;
+
+  const minWidth = 80;
+  const maxWidth = 130;
+  const width = minWidth + Math.random() * (maxWidth - minWidth);
+
+  const platformWithGorilla = index === 1 || index === 6;
+
+  const minHeight = 40;
+  const maxHeight = 300;
+  const minHeightGorilla = 30;
+  const maxHeightGorilla = 150;
+
+  const height = platformWithGorilla
+    ? minHeightGorilla + Math.random() * (maxHeightGorilla - minHeightGorilla)
+    : minHeight + Math.random() * (maxHeight - minHeight);
+
+  const lightsOn = [];
+  for (let i = 0; i < 50; i++) {
+    const light = Math.random() <= 0.33 ? true : false;
+    lightsOn.push(light);
+  }
+
+  state.backgroundBuildings.push({ x, width, height, lightsOn });
+}
+
+function draw() {
+  ctx.save();
+
+  //Flip coordinate system upside down allowing y-axis to increase upwards
+  ctx.translate(0, canvas.innerHeight);
+  ctx.scale(1, -1);
+
+  // Draw scene
+  drawBackground();
+  drawBackgroundBuildings();
+  drawBuildings();
+  drawGorilla(1);
+  drawGorilla(2);
+  drawBomb();
+
+  //Restore transformation
+  ctx.restore();
+}
+
+function drawBackground() {
+  const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+  gradient.addColorStop(1, "#F8BA85");
+  gradient.addColorStop(0, "#FFC28E");
+
+  // Draw sky
+  ctx.fillStyle = gradient;
+  ctx.fillRect(0, 0, canvas.innerWidth, canvas.innerHeight);
+
+  // Draw sun/moon
+  ctx.fillStyle = "rgba(255, 255, 255, 0.6)";
+  ctx.beginPath();
+  ctx.arc(300, 350, 60, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function drawBackgroundBuildings() {
+  state.backgroundBuildings.forEach((building) => {
+    ctx.fillStyle = "#947285";
+    ctx.fillRect(building.x, 0, building.width, building.height);
+  });
+}
+
+function drawBuildings() {
+  state.buildings.forEach((building) => {
+    ctx.fillStyle = "#4A3C68";
+    ctx.fillRect(building.x, 0, building.width, building.height);
+
+    // Draw windows
+    const windowWidth = 10;
+    const windowHeight = 12;
+    const gap = 15;
+
+    const numberOfFloors = Math.ceil(
+      (building.height - gap) / (windowHeight + gap)
+    );
+
+    const numberOfRoomsPerFloor = Math.floor(
+      (building.width - gap) / (windowWidth + gap)
+    );
+
+    for (let floor = 0; floor < numberOfFloors; floor++) {
+      for (let room = 0; room < numberOfRoomsPerFloor; room++) {
+        if (building.lightsOn[floor * numberOfRoomsPerFloor + room]) {
+          ctx.save();
+
+          ctx.translate(building.x + gap, buidling.height - gap);
+          ctx.scale(1, -1);
+
+          const x = room * (windowWidth + gap);
+          const y = floor * (windowHeight + gap);
+
+          ctx.fillStyle = "#EBB6A2";
+          ctx.fillRect(x, y, windowWidth, windowHeight);
+
+          ctx.restore();
+        }
+      }
+    }
+  });
+}
+
+function drawGorilla(player) {
+  ctx.save();
+
+  // Determine which building the gorilla is on
+  const building =
+    player === 1 ? state.buildings.at(1) : state.buildings.at(-2);
+
+  ctx.translate(building.x + building.width / 2, building.height);
+
+  // draw Gorilla
+  drawGorillaBody();
+  drawGorillaLeftArm(player);
+  drawGorillaRightArm(player);
+  drawGorillaFace(player);
+
+  ctx.restore();
+}
+
+function drawGorillaBody() {
+  ctx.fillStyle = "#000000";
+
+  ctx.beginPath();
+  ctx.moveTo(0, 15);
+  ctx.lineTo(-7, 0);
+  ctx.lineTo(-20, 0);
+  ctx.lineTo(-17, 18);
+  ctx.lineTo(-20, 44);
+
+  ctx.lineTo(-11, 77);
+  ctx.lineTo(0, 84);
+  ctx.lineTo(11, 77);
+
+  ctx.lineTo(20, 44);
+  ctx.lineTo(17, 18);
+  ctx.lineTo(20, 0);
+  ctx.lineTo(7, 0);
+  ctx.fill();
+}
+
+function drawGorillaLeftArm(player) {
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = 18;
+
+  ctx.beginPath();
+  ctx.moveTo(-14, 50);
+
+  if (state.phase === "aiming" && state.currentPlayer === 1 && player === 1) {
+    ctx.quadraticCurveTo(-44, 63, -28, 107);
+  } else if (state.phase === "celebrating" && state.currentPlayer === player) {
+    ctx.quadraticCurveTo(-44, 63, -28, 107);
+  } else {
+    ctx.quadraticCurveTo(-44, 45, -28, 12);
+  }
+
+  ctx.stroke();
+}
+
+function drawGorillaRightArm(player) {
+  ctx.strokeStyle = "#000000";
+  ctx.lineWidth = 18;
+
+  ctx.beginPath();
+  ctx.moveTo(-14, 50);
+
+  if (state.phase === "aiming" && state.currentPlayer === 2 && player === 2) {
+    ctx.quadraticCurveTo(44, 63, 28, 107);
+  } else if (state.phase === "celebrating" && state.currentPlayer === player) {
+    ctx.quadraticCurveTo(44, 63, 28, 107);
+  } else {
+    ctx.quadraticCurveTo(44, 45, 28, 12);
+  }
+
+  ctx.stroke();
+}
